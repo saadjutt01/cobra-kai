cc047af18f368988494e6780d7fa604357da9ad5
Saad Jutt <ihsan.distranger@gmail.com>
2020-12-14 04:43:41 +0500

    localization + fonts added

---
+++ js/localizations.js
@@ -0,0 +1,32 @@
+String.toLocaleString({
+  en: {
+    "%play-game": "Play Game",
+    "%watch-trailer": "Watch Trailer",
+    "%privacy": "Privacy",
+    "%imprint": "Imprint",
+    "%watch-on-netflix": "Watch on netflix now",
+    "%popup-text":
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
+    "%concept-by": "Concept, development and hosting by",
+  },
+  de: {
+    "%play-game": "Spiel spielen",
+    "%watch-trailer": "Trailer ansehen",
+    "%privacy": "Privatsphäre",
+    "%imprint": "Impressum",
+    "%watch-on-netflix": "Beobachten Sie auf Netflix",
+    "%popup-text":
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus Commodo Viverra Maecenas Accumsan Lacus Vel Facilisis. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus Commodo Viverra Maecenas Accumsan Lacus Vel Facilisis. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus Commodo Viverra Maecenas Accumsan Lacus Vel Facilisis. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus Commodo Viverra Maecenas Accumsan Lacus Vel Facilisis.",
+    "%concept-by": "Konzept, Entwicklung und Hosting von",
+  },
+});
+
+const translations = [
+  "play-game",
+  "watch-trailer",
+  "privacy",
+  "imprint",
+  "watch-on-netflix",
+  "popup-text",
+  "concept-by",
+];
